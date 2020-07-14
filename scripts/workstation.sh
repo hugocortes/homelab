bf214cd1d5545faeecebc7cabb30cd3df6a70604
Hugo Cortes <contact@hugocortes.dev>
2020-07-14 08:01:04 -0700

    feat(role): workstation configuration

---
+++ scripts/workstation.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo "running setup..."
+
+GITHUB_DIR=~/code/github.com/hugocortes/homelab
+if [ ! -d $GITHUB_DIR ]; then
+  echo "fetching github repository"
+  mkdir -p ~/code/github.com/hugocortes
+  git clone https://github.com/hugocortes/homelab.git $GITHUB_DIR
+fi
+
+if [ -f /etc/lsb-release ]; then
+  sudo apt-get update -y
+  sudo apt-get upgrade -y
+  sudo apt install ansible -y
+fi
+
+cd $GITHUB_DIR
+if [ ! -f inventory/group_vars/all/all.yaml ]; then
+  scp inventory/group_vars/all/all.yaml.example inventory/group_vars/all/all.yaml
+fi
+if [ ! -f inventory/group_vars/all/kubespray.yml ]; then
+  scp inventory/group_vars/all/kubespray.yml.example inventory/group_vars/all/kubespray.yml
+fi
+if [ ! -f inventory/hosts.ini ]; then
+  scp inventory/hosts.example.ini inventory/hosts.ini
+fi
